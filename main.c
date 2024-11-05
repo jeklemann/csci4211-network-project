d6733a7e98f5edf8baa36c4c772d523af1bbbc50
Julian Klemann <klema038@umn.edu>
2024-11-05 00:11:02 -0600

    Add hash table tests
    Move this later to its own testing area.

--- main.c
+++ main.c
@@ -5,28 +5,43 @@
 
 void test_hash(void)
 {
-    char *key1 = "penis";
-    char *val1 = "cum";
-    char *key2 = "bruh";
-    char *val2 = "real";
-    char buf[256];
-
     struct hash_table *table = hash_init(16);
-    printf("%d\n", hash_insert(table, key1, strlen(key1) + 1, val1, strlen(val1) + 1));
-    printf("%d\n", hash_insert(table, key2, strlen(key2) + 1, val2, strlen(val2) + 1));
-    printf("%d\n", hash_get(table, key1, strlen(key1) + 1, buf, strlen(val1)));
-    printf("%d\n", hash_get(table, key1, strlen(key1) + 1, buf, 256));
-    printf("%s\n", buf);
-    printf("%d\n", hash_get(table, key2, strlen(key2) + 1, buf, 256));
-    printf("%s\n", buf);
+    char *key1 = "foo", *val1 = "bar";
+    char *key2 = "foobar", *val2 = "quux";
+    char buf[256];
+    int ret;
 
-    printf("%d\n", hash_delete(table, key1, strlen(key1) + 1));
-    printf("%d\n", hash_delete(table, key2, strlen(key2) + 1));
+    ret = hash_insert(table, key1, strlen(key1) + 1, val1, strlen(val1) + 1);
+    printf("expected: %d, got: %d\n", HASH_OK, ret);
+    ret = hash_insert(table, key1, strlen(key1) + 1, val1, strlen(val1) + 1);
+    printf("expected: %d, got: %d\n", HASH_ERR_KEY_EXISTS, ret);
+    ret = hash_insert(table, key2, strlen(key2) + 1, val2, strlen(val2) + 1);
+    printf("expected: %d, got: %d\n", HASH_OK, ret);
+
+    ret = hash_get(table, key1, strlen(key1) + 1, buf, strlen(val1));
+    printf("expected: %d, got: %d\n", HASH_ERR_BUFFER_TOO_SMALL, ret);
+    ret = hash_get(table, key1, strlen(key1) + 1, buf, sizeof(buf));
+    printf("expected: %d, got: %d\n", HASH_OK, ret);
+    printf("expected value: %s, got value: %s\n", val1, buf);
+    ret = hash_get(table, key2, strlen(key2) + 1, buf, sizeof(buf));
+    printf("expected: %d, got: %d\n", HASH_OK, ret);
+    printf("expected value: %s, got value: %s\n", val2, buf);
+    ret = hash_get(table, val1, strlen(val1) + 1, buf, sizeof(buf));
+    printf("expected: %d, got: %d\n", HASH_ERR_KEY_DOES_NOT_EXIST, ret);
+
+    ret = hash_delete(table, key1, strlen(key1) + 1);
+    printf("expected: %d, got: %d\n", HASH_OK, ret);
+    ret = hash_delete(table, key1, strlen(key1) + 1);
+    printf("expected: %d, got: %d\n", HASH_ERR_KEY_DOES_NOT_EXIST, ret);
+    ret = hash_delete(table, key2, strlen(key2) + 1);
+    printf("expected: %d, got: %d\n", HASH_OK, ret);
+
     hash_free(table);
 }
 
 int main(int argc, char **argv)
 {
+    test_hash();
     printf("Starting mqttd\n");
     return 0;
 }
