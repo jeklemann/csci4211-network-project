0f257d848f97456c521860b846a092868793102e
Julian Klemann <klema038@umn.edu>
2024-11-10 03:14:06 -0600

    Add command line arguments

--- src/main.c
+++ src/main.c
@@ -1,10 +1,50 @@
+#include <limits.h>
 #include <stdio.h>
 
 #include "server.h"
 
+enum
+{
+    DEFAULT_PORT = 1883,
+};
+
+void usage()
+{
+    printf("Usage: mqttd [port]\n");
+    exit(EXIT_FAILURE);
+}
+
 int main(int argc, char **argv)
 {
-    printf("Starting mqttd\n");
-    start_server(8024);
+    int p;
+
+    if (argc > 2)
+        usage();
+
+    if (argc == 2)
+        p = atoi(argv[1]);
+    else
+        p = DEFAULT_PORT;
+
+    if (!p)
+    {
+        printf("invalid port\n");
+        usage();
+    }
+
+    if (p > USHRT_MAX)
+    {
+        printf("port is too high\n");
+        usage();
+    }
+
+    if (p < 1024)
+    {
+        printf("port should not be below 1024\n");
+        usage();
+    }
+
+    printf("Starting mqttd on port %hu\n", p);
+    start_server(p);
     return 0;
 }
