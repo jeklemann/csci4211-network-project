8c8395cd6329b277061316a27dd6061a288228d7
Julian Klemann <klema038@umn.edu>
2024-11-11 04:00:01 -0600

    Implement subscriptions

--- include/server.h
+++ include/server.h
@@ -11,4 +11,18 @@ struct connection
     char *name;
 };
 
+struct topic
+{
+    struct list entry;
+    char *name;
+    struct hash_table *subs;
+    pthread_mutex_t subs_lock;
+};
+
+struct subscription
+{
+    struct list entry;
+    char *client_name;
+};
+
 void start_server(unsigned short port);
--- src/server.c
+++ src/server.c
@@ -11,9 +11,18 @@
 #include "server.h"
 #include "utils.h"
 
+static char *DEFAULT_TOPIC_NAMES[] = {
+    "WEATHER",
+    "NEWS",
+};
+
+/* TODO: Queue messages, Phase 1 does not include this */
+
 /* Connected clients */
 static struct hash_table *online_clients;
 pthread_mutex_t online_lock = PTHREAD_MUTEX_INITIALIZER;
+
+static struct hash_table *topics;
 
 static void close_connection(struct connection *conn)
 {
@@ -26,11 +35,90 @@ static void close_connection(struct connection *conn)
     free(conn);
 }
 
+static struct topic *get_topic(char *name)
+{
+    struct list *bucket, *cur;
+    struct topic *topic;
+    size_t hash;
+
+    hash = hash_bytes(name, strlen(name) + 1) % topics->size;
+    bucket = &topics->buckets[hash];
+
+    cur = bucket->next;
+    while (cur != bucket)
+    {
+        topic = LIST_ENTRY(cur, struct topic, entry);
+        if (!strcmp(topic->name, name))
+            return topic;
+    }
+
+    return NULL;
+}
+
+/* Must lock online_lock when calling */
+static struct connection *get_client_by_name(char *name)
+{
+    struct list *bucket, *cur;
+    struct connection *conn;
+    size_t hash;
+
+    hash = hash_bytes(name, strlen(name) + 1) % topics->size;
+    bucket = &topics->buckets[hash];
+
+    cur = bucket->next;
+    while (cur != bucket)
+    {
+        conn = LIST_ENTRY(cur, struct connection, entry);
+        if (!strcmp(conn->name, name))
+            return conn;
+    }
+
+    return NULL;
+}
+
+/* Must lock topic->subs_lock */
+static int exists_sub_by_name(struct topic *topic, char *name)
+{
+    struct list *bucket, *cur;
+    struct subscription *sub;
+    size_t hash;
+
+    hash = hash_bytes(name, strlen(name) + 1) % topics->size;
+    bucket = &topics->buckets[hash];
+
+    cur = bucket->next;
+    while (cur != bucket)
+    {
+        sub = LIST_ENTRY(cur, struct subscription, entry);
+        if (!strcmp(sub->client_name, name))
+            return 1;
+    }
+
+    return 0;
+}
+
+static void reply_conn(struct connection *conn, char *msg, size_t msg_len)
+{
+    int res;
+
+    res = send(conn->sock, msg, msg_len, 0);
+    if (res == -1 && (errno == ENOTCONN || errno == ECONNRESET))
+    {
+        perror("send");
+        conn->closing = 1;
+    }
+    else if (!res)
+    {
+        conn->closing = 1;
+    }
+
+    return;
+}
+
 static void connect_command(struct connection *conn, char **cmd_toks, size_t num_toks)
 {
     static char *CONN_ACK = "CONN_ACK";
     char *name;
-    int res;
 
     if (num_toks < 2)
         return; /* Specification does not demand we respond */
@@ -44,22 +132,20 @@ static void connect_command(struct connection *conn, char **cmd_toks, size_t num
 
     pthread_mutex_lock(&online_lock);
 
+    if (get_client_by_name(name))
+    {
+        /* This name is taken, we have no defined error in the standard though */
+        pthread_mutex_unlock(&online_lock);
+        return;
+    }
+
     list_remove(&conn->entry); /* In case of resending the CONN command */
     conn->name = name;
     hash_insert(online_clients, conn->name, strlen(conn->name) + 1, &conn->entry);
 
     pthread_mutex_unlock(&online_lock);
 
-    res = send(conn->sock, CONN_ACK, strlen(CONN_ACK), 0);
-    if (res == -1 && (errno == ENOTCONN || errno == ECONNRESET))
-    {
-        perror("send");
-        conn->closing = 1;
-    }
-    else if (!res)
-    {
-        conn->closing = 1;
-    }
+    reply_conn(conn, CONN_ACK, strlen(CONN_ACK));
 
     return;
 }
@@ -74,10 +160,59 @@ static void publish_command(struct connection *conn, char **cmd_toks, size_t num
 
 static void subscribe_command(struct connection *conn, char **cmd_toks, size_t num_toks)
 {
-    printf("Sub\n");
+    static char *NOT_FOUND = "<ERROR: Subscription Failed - Subject Not Found>";
+    static char *SUB_ACK = "<SUB_ACK>";
+    struct subscription *sub;
+    char *name, *topic_name;
+    struct topic *topic;
 
     if (num_toks < 3)
         return; /* Specification does not demand we respond */
+
+    name = cmd_toks[0];
+    topic_name = cmd_toks[2];
+    topic = get_topic(topic_name);
+    if (!topic)
+    {
+        reply_conn(conn, NOT_FOUND, strlen(NOT_FOUND));
+        return;
+    }
+
+    pthread_mutex_lock(&topic->subs_lock);
+
+    if (exists_sub_by_name(topic, name))
+    {
+        /* Already subscribed, just ACK */
+        reply_conn(conn, SUB_ACK, strlen(SUB_ACK));
+        pthread_mutex_unlock(&topic->subs_lock);
+        return;
+    }
+
+    sub = malloc(sizeof(*sub));
+    if (!sub)
+    {
+        perror("malloc");
+        pthread_mutex_unlock(&topic->subs_lock);
+        return;
+    }
+
+    list_init(&sub->entry);
+    sub->client_name = strdup(name);
+    if (!sub->client_name)
+    {
+        perror("malloc");
+        free(sub);
+        pthread_mutex_unlock(&topic->subs_lock);
+        return;
+    }
+
+    hash_insert(topic->subs, sub->client_name, strlen(sub->client_name) + 1, &sub->entry);
+
+    pthread_mutex_unlock(&topic->subs_lock);
+
+    reply_conn(conn, SUB_ACK, strlen(SUB_ACK));
+
+    return;
 }
 
 static void disconnect_command(struct connection *conn, char **cmd_toks, size_t num_toks)
@@ -165,6 +300,50 @@ static void *handle_connection(void *data)
     return NULL;
 }
 
+static void init_topics()
+{
+    size_t i, num_topics = sizeof(DEFAULT_TOPIC_NAMES) / sizeof(*DEFAULT_TOPIC_NAMES);
+    struct topic *topic;
+
+    topics = hash_init(8);
+    if (!topics)
+        exit(EXIT_FAILURE);
+
+    for (i = 0; i < num_topics; i++)
+    {
+        topic = malloc(sizeof(*topic));
+        if (!topic)
+        {
+            perror("malloc");
+            free(topics);
+            exit(EXIT_FAILURE);
+        }
+
+        topic->name = strdup(DEFAULT_TOPIC_NAMES[i]);
+        if (!topic->name)
+        {
+            perror("strdup");
+            free(topics);
+            free(topic);
+            exit(EXIT_FAILURE);
+        }
+
+        pthread_mutex_init(&topic->subs_lock, NULL);
+        list_init(&topic->entry);
+        topic->subs = hash_init(16);
+        if (!topic->subs)
+        {
+            perror("strdup");
+            free(topic->name);
+            free(topic);
+            free(topics);
+            exit(EXIT_FAILURE);
+        }
+
+        hash_insert(topics, topic->name, strlen(topic->name) + 1, &topic->entry);
+    }
+}
+
 void start_server(unsigned short port)
 {
     int sock, conn_sock, thread_ret;
@@ -179,6 +358,8 @@ void start_server(unsigned short port)
         perror("hash_init");
         exit(EXIT_FAILURE);
     }
+
+    init_topics();
 
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
